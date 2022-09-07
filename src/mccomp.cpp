96f643241982c8d4ca13bee487b53d3fe290e441
msd <87383399+msd@users.noreply.github.com>
2022-09-07 02:40:27 +0100

    Removed unnecessary includes

--- src/mccomp.cpp
+++ src/mccomp.cpp
@@ -1,41 +1,11 @@
-#include "llvm/ADT/APFloat.h"
-#include "llvm/ADT/Optional.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/GlobalVariable.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/Verifier.h"
-#include "llvm/Support/FileSystem.h"
-#include "llvm/Support/Host.h"
-#include "llvm/MC/TargetRegistry.h"
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Target/TargetMachine.h"
-#include "llvm/Target/TargetOptions.h"
-#include <algorithm>
-#include <cassert>
-#include <cctype>
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
 #include <map>
-#include <memory>
 #include <queue>
-#include <string.h>
-#include <string>
-#include <system_error>
-#include <utility>
-#include <vector>
-#include <stdexcept>
 #include <stack>
+
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/Support/FileSystem.h"
 
 using namespace llvm;
 using namespace llvm::sys;
