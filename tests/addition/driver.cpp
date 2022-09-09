c0a53635022d5660f5bc42b6ea5484dadc050941
msd <87383399+msd@users.noreply.github.com>
2022-09-09 22:35:24 +0100

    Fixed addition test

--- tests/addition/driver.cpp
+++ tests/addition/driver.cpp
@@ -9,23 +9,34 @@
 #define DLLEXPORT
 #endif
 
-extern "C" DLLEXPORT int print_int(int X) {
-  fprintf(stderr, "%d\n", X);
-  return 0;
+extern "C" DLLEXPORT int print_int(int X)
+{
+	fprintf(stderr, "%d\n", X);
+	return 0;
 }
 
-extern "C" DLLEXPORT float print_float(float X) {
-  fprintf(stderr, "%f\n", X);
-  return 0;
+extern "C" DLLEXPORT float print_float(float X)
+{
+	fprintf(stderr, "%f\n", X);
+	return 0;
 }
 
 extern "C" {
-    int addition(int n, int m);
+	int addition(int, int);
 }
 
-int main() {
-    if(addition(6, 3) == 9) 
-      std::cout << "PASSED Result: " << addition(6, 3) << std::endl;
-  	else 
-  	  std::cout << "FALIED Result: " << addition(6, 3) << std::endl;
+using namespace std;
+
+int main()
+{
+	int expected = 7;
+	int got = addition(3, 4);
+	if (expected == got)
+	{
+		cout << "[SUCCESS] addition " << endl;
+	}
+	else
+	{
+		cout << "[FAILURE] addition EXPECTED: " << expected << " GOT: " << got << endl;
+	}
 }
